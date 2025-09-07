0edaf602d56707ca9d8fa49fc105bd036e27b0c9
andrewfx2 <109618359+andrewfx2@users.noreply.github.com>
2025-09-07 00:04:01 -0400

    Create hockey-widget-loader.js

---
+++ hockey-widget-loader.js
@@ -0,0 +1,40 @@
+(function() {
+    const script = document.currentScript;
+    const widgetType = script.getAttribute('data-widget');
+    const containerId = script.getAttribute('data-container');
+    const baseUrl = 'https://yourusername.github.io/hockey-widgets';
+    
+    if (window.HockeyCardWidget) {
+        initializeWidget();
+        return;
+    }
+    
+    Promise.all([
+        loadScript(baseUrl + '/widget-configs.js'),
+        loadScript(baseUrl + '/hockey-widget-system.js'),
+        loadCSS(baseUrl + '/hockey-widget-styles.css')
+    ]).then(initializeWidget);
+    
+    function initializeWidget() {
+        const config = window.getHockeyWidgetConfig(widgetType);
+        if (config) {
+            window.initHockeyWidget(containerId, config);
+        }
+    }
+    
+    function loadScript(src) {
+        return new Promise((resolve) => {
+            const script = document.createElement('script');
+            script.src = src;
+            script.onload = resolve;
+            document.head.appendChild(script);
+        });
+    }
+    
+    function loadCSS(href) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = href;
+        document.head.appendChild(link);
+    }
+})();
